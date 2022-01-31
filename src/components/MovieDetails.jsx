9ecef5260aee2546d8328bb54ab5a47181fe5c6b
Nicolas <ntroccaz@gmail.com>
2022-02-01 00:51:38 +0100

    details popup icons & toggle added

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,16 +1,28 @@
 import React from "react";
+import { useState } from "react/cjs/react.development";
+import { ReactComponent as CloseIcon } from "./../assets/close_black_24dp.svg";
+import { ReactComponent as FavIconOn } from "./../assets/favorite_black_24dp.svg";
+import { ReactComponent as FavIconOff } from "./../assets/favorite_border_black_24dp.svg";
 
 function MovieDetails(props) {
+  const [isFavOn, setFavOn] = useState("false");
+  const ToggleFav = () => {
+    setFavOn(!isFavOn);
+  };
+
   return props.trigger ? (
     <div className="MovieDetails">
       <div className="popup">
-        <button className="close-btn" onClick={() => props.setTrigger(false)}>
-          X
-        </button>
-
+        <div className="close-btn" onClick={() => props.setTrigger(false)}>
+          <CloseIcon />
+        </div>
         {props.children}
-
-        <button className="favorite-btn">Favorite add/remove</button>
+        <div className={isFavOn ? "favorite-btn-on" : "favorite-btn-off"}>
+          {isFavOn && <FavIconOn onClick={() => ToggleFav()} />}
+          {isFavOn && " remove from favorites"}
+          {!isFavOn && <FavIconOff onClick={() => ToggleFav()} />}
+          {!isFavOn && " add to favorites"}
+        </div>
       </div>
     </div>
   ) : (
