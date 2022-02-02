95ea01628bc98a033a483bccc6a8b7a3f110c5d6
Nicolas <ntroccaz@gmail.com>
2022-02-02 23:37:27 +0100

    search callback added

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,32 +1,30 @@
-import React, { Component, useState } from "react";
-import { ReactComponent as SearchIcon } from "./../assets/search_black_24dp.svg";
-const Search = () => {
-  const [Query, setQuery] = useState("");
-  const [Results, setResults] = useState([]);
-  const [Loading, setLoading] = useState("false");
-  const [ErrorMessage, setErrorMessage] = useState("");
+import React, { useState } from "react";
 
-  function handleOnInputChange(event) {
-    const query = event.nativeEvent.data;
-    setQuery(Query + query);
-    console.log(Query);
-    setLoading("true");
-    setErrorMessage("");
-  }
+const Search = (props) => {
+  const [query, setQuery] = useState("");
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      //alert("Search query = " + query);
+      props.searchCallback(query);
+    }
+  };
+
+  const handleChange = (event) => {
+    setQuery(event.target.value);
+    //console.log(query);
+  };
 
   return (
     <div className="search-bar">
-      {/*Search Input*/}
-      <label className="search-label" htmlFor="search-input">
-        <input
-          type="search"
-          value={Query}
-          id="search-input"
-          placeholder="Search movie..."
-          onChange={handleOnInputChange}
-        />
-        <SearchIcon />
-      </label>
+      <input
+        type="search"
+        value={query}
+        placeholder="Search movie..."
+        onChange={handleChange}
+        onKeyPress={handleKeyPress}
+      />
     </div>
   );
 };
